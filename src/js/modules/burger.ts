e9e12ac8c74fca397026c2da3c219015d7069874
Semyon Surkov <semyonsurkov@outlook.com>
2023-06-06 11:35:36 +0300

    Make edits for burger

--- src/js/modules/burger.ts
+++ src/js/modules/burger.ts
@@ -1,13 +1,14 @@
 export const burger = (menuSelector: string, burgerSelector: string): void => {
   const menuElement = document.querySelector(menuSelector) as HTMLElement; 
   const burgerElement = document.querySelector(burgerSelector) as HTMLElement; 
+  const BREAKPOINT = 992;
 
   menuElement.style.display = 'none'; 
 
   burgerElement?.addEventListener('click', () => {
     if (
       menuElement.style.display === 'none' &&
-      window.screen.availWidth < 993
+      window.screen.availWidth < BREAKPOINT
     ) {
       menuElement.style.display = 'block';
     } else {
@@ -16,7 +17,7 @@ export const burger = (menuSelector: string, burgerSelector: string): void => {
   });
 
   window.addEventListener('resize', () => {
-    if (window.screen.availWidth > 992) {
+    if (window.screen.availWidth > BREAKPOINT) {
       menuElement.style.display = 'none';
     }
   });
